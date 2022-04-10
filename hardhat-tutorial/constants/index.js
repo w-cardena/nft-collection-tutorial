4291f07051ad0064cc085778b1d3edb005e90a39
w-cardena <frinkwave2030@gmail.com>
2022-04-10 14:31:45 +0200

    commit on master

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x617A973847040fB724105A25b78b5b85752488f7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
